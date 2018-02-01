d276b153cc9034b446f12db22c7dee31a1d7fbf7
felicia-11 <feliciachristiany@yahoo.com>
2018-02-01 07:25:05 +0700

    Update layout

--- app/src/main/java/com/example/hizkia/train_tracker_kv2/MainActivity.java
+++ app/src/main/java/com/example/hizkia/train_tracker_kv2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hizkia.train_tracker_kv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner curr = (Spinner) findViewById(R.id.spinnerCurrent);
+
+        ArrayAdapter<String> currAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.listCurrent));
+        currAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        curr.setAdapter(currAdapter);
+
+        Spinner dest = (Spinner) findViewById(R.id.spinnerDestination);
+
+        ArrayAdapter<String> destAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.listDestination));
+        destAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dest.setAdapter(destAdapter);
     }
 }
